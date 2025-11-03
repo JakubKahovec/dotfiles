c30c7708f9dbd224c5ec66b13a8107dc71cec4ca
Jakub Kahovec <jakub.kahovec@gmail.com>
2025-11-03 09:01:56 +0100

    Add neovim config files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,33 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("config.lazy")
+
+vim.cmd[[colorscheme tokyonight-storm]]
+
+vim.lsp.config('*', {
+  capabilities = {
+    textDocument = {
+      semanticTokens = {
+        multilineTokenSupport = true,
+      }
+    }
+  },
+  root_markers = { '.git' },
+})
+
+vim.diagnostic.config({ virtual_text = true })
+
+vim.keymap.set("n", "=", vim.lsp.buf.format, { remap = false })
+
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+
+vim.wo.number = true
+
+vim.api.nvim_set_keymap("n", "<localleader>rr", "<cmd>!uv run %<CR>", { noremap = false, silent = true })
+
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.api.nvim_set_option("clipboard", "unnamedplus")
